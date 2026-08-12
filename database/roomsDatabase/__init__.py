6d94dc6fb24ba7efd5f74b9428c5c3f22666dbec
guilhermefrmdev <guilhermefrmdev@gmail.com>
2026-08-12 12:57:19 -0300

    feat: criado o arquivo __init__.py e o módulo de cadastrar quarto

---
+++ database/roomsDatabase/__init__.py
@@ -0,0 +1,12 @@
+# Verificação se o banco de dados está criado
+try:
+    nome_arquivo = './database/roomsDatabase/roomsDatabase.csv'
+    arquivo = open(nome_arquivo, 'r+')
+except FileNotFoundError:   # Se não existente, criar
+    arquivo = open(nome_arquivo, 'w+')
+    arquivo.writelines('number,name,capacity,daily_value,free\n')
+arquivo.close()
+
+from .cadastrarQuarto import *
+
+__all__ = ['cadastrarQuarto']
---
+++ database/roomsDatabase/cadastrarQuarto.py
@@ -0,0 +1,63 @@
+from csv import writer
+
+# Exceptions customizadas para melhor tratamento de erros
+class RoomNumberNotInt(Exception):
+    pass
+
+class RoomNumberZero(Exception):
+    pass
+
+class NameNotStr(Exception):
+    pass
+
+class EmptyName(Exception):
+    pass
+
+class RoomCapacityNotInt(Exception):
+    pass
+
+class RoomCapacityZero(Exception):
+    pass
+
+class DailyValueNotFloat(Exception):
+    pass
+
+class RoomDailyValueZero(Exception):
+    pass
+
+class FreeNotBool(Exception):
+    pass
+
+def cadastrarQuarto(number: int,name: str,capacity: int,daily_value: float,free: bool) -> None:
+    # Verificações number
+    if not isinstance(number, int):
+        raise RoomNumberNotInt("Número do quarto não é inteiro")
+    if number == 0:
+        raise RoomNumberZero("Número do quarto não pode ser Zero")
+
+    # Verificações name
+    if not isinstance(name, str):
+        raise NameNotStr("O nome do quarto não é Str")
+    if name == '':
+        raise EmptyName("O nome não pode ser vazio")
+
+    # Verificações capacity
+    if not isinstance(capacity, int):
+        raise RoomCapacityNotInt("A capacidade do quarto não é inteiro")
+    if capacity == 0:
+        raise RoomCapacityZero("A capacidade do quarto não pode ser Zero")
+
+    # Verificações daily_value
+    if not isinstance(daily_value, float):
+        raise DailyValueNotFloat("O valor do quarto não é float")
+    if capacity == 0.0:
+        raise RoomDailyValueZero("O valor do quarto não pode ser Zero")
+
+    # Verificações free
+    if not isinstance(free, bool):
+        raise FreeNotBool("Livre não é um boleano")
+
+    # Fazer o cadastro do quarto
+    with open("./database/roomsDatabase/roomsDatabase.csv", "a", newline='') as database:
+        write = writer(database)
+        write.writerow([number, name, capacity, daily_value, free])
