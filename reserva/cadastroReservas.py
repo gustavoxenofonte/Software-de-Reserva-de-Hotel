96ae775a839ccca9d7ff2f0fb1fa9d0b22ff2655
Robson Arthur <robsonarthur995@gmail.com>
2026-08-13 21:36:46 -0300

    feat/ alterando a função cadastroReserva

--- reserva/cadastroReservas.py
+++ reserva/cadastroReservas.py
@@ -15,37 +15,62 @@ class DiasReservadosInvalidos(Exception):
 class NumeroQuartoInvalido(Exception):
      pass
 
+class CpfInvalido(Exception):
+     pass
+
+class TelefoneInvalido(Exception):
+     pass
+
+class DataCheckinInvalida(Exception):
+     pass
+
+class DataCheckoutInvalida(Exception):
+     pass
 
 def cadastroReserva():
-    try:
         nomeHospede = str(input("Insira o nome do hóspede que deseja fazer a reserva: ")).strip().title()
 
         cpfHospede = str(input("Insira o CPF do hóspede: ")).strip()
         cpfHospede = "".join(x for x in cpfHospede if x.isnumeric()) ##essa função .join() retira todo caracter que não for númerico da str, como !., e etc.
+        while len(cpfHospede) != 11:  ##confere se o cpf é real
+            cpfHospede = str(input("Insira o CPF do hóspede: ")).strip()
+            cpfHospede = "".join(x for x in cpfHospede if x.isnumeric())
+             
 
         telefoneHospede = str(input("Insira o telefone do hóspede: ")).strip()
         telefoneHospede = "".join(x for x in telefoneHospede if x.isnumeric()) ##realiza o mesmo que na variável cpf
+        while len(telefoneHospede) != 11:
+            telefoneHospede = str(input("Insira o telefone do hóspede: ")).strip()
+            telefoneHospede = "".join(x for x in telefoneHospede if x.isnumeric())
+             
+        while True:
+            try:
+                idadeHospede = int(input("Insira a idade do hóspede: "))
+                while idadeHospede <= 0:
+                    idadeHospede = int(input("Insira a idade do hóspede: "))
+            except ValueError:
+                 print("Valor inválido")
+                 break
+                 
+                     
+                        
 
-        idadeHospede = int(input("Insira a idade do hóspede: "))
-        if idadeHospede <= 0:
-                raise IdadeInvalida("Idade inválida")
         
-        diasReservados = int(input("Insira por quantos dias deseja reservar o quarto: "))
-        if diasReservados <= 0:
-                raise DiasReservadosInvalidos("Dias reservados inválidos")
-
         numeroQuarto = int(input("Número do quarto a ser reservado: "))
         if numeroQuarto <= 0:
                 raise NumeroQuartoInvalido("Número do quarto inválido")
 
         try:
-            diaCheckin = int(input("Insira o dia de Check-in do hóspede: "))
-            mesCheckin = int(input("Insira o mês de Check-in do hóspede(digite 0 para o mês atual): "))
-            anoCheckin = int(input("Insira o ano de Check-in do hóspede(digite 0 para o ano atual): "))
-            dataCheckin = datetime.date(anoCheckin,mesCheckin,diaCheckin)
+            dataCheckin = str(input("Insira o a data de check-in(DD/MM/YYYY): ")).strip()
+            dataCheckin = datetime.date.strptime(f"{dataCheckin}", '%d/%m/%Y')    #transforma o str que o usuário digitou em uma variável do tipo date
+            if dataCheckin <= datetime.date.today():                                 #verifica se a data de check-in é válida(apartir do próximo dia)
+                raise DataCheckinInvalida("Data de Chek-in inválida")
+            
+            dataCheckout = str(input("Insira o a data de check-on(DD/MM/YYYY): ")).strip()
+            dataCheckout = datetime.date.strptime(dataCheckout, "%d/%m/%Y")
+            if dataCheckout < dataCheckin:                       #verifica se a data de check-out é antes da data de check-in, se sim, retorna um erro
+
         except ValueError:
             raise DataInvalida("Data inválida")
         
-    except ValueError:
-        print("Valor inválido inserido.")
-        return False
+cadastroReserva()
