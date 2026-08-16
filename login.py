a952bb7e46cad28037e49086839bedd2124040ef
gustavoxenofonte <gustavoxenofonte@gmail.com>
2026-08-16 14:29:32 -0300

    feat/sistema-de-login-inicial

--- login.py
+++ login.py
@@ -1,13 +1,26 @@
 from database.adminDatabase.adminExiste import *
 from database.adminDatabase.cadastrarLoginAdministrativo import *
-import sys
+from sys import exit
+from pathlib import Path
+from csv import DictReader
 
 def login_adm():
+    print("== TELA DE LOGIN ==")
     if adminExiste() == True:
-        pass
+        usuario = str(input("Usuário: "))
+        senha = str(input("Senha: "))
+        caminho = Path("database/adminDatabase/adminDatabase.csv")
+
+        with open(caminho, 'r', encoding='utf-8') as arquivo:
+            leitor = DictReader(arquivo)
+            for linha in leitor:
+                if linha["user_name"] == usuario and linha["password"] == senha:
+                    print("Deu certo")
+                else:
+                    print("Nao deu certo")
+
     else:
         while True:
-            print("== TELA DE LOGIN == ")
             opcao = input("Não existe nenhum cadastro de login administrativo, deseja criar um? (S/N) ")
             if opcao.upper() == "S":
                 usuario = str(input("Usuário: "))
@@ -15,6 +28,6 @@ def login_adm():
                 cadastrarLoginAdministrativo(usuario, senha)
                 break
             elif opcao.upper() == "N":
-                sys.exit()
+                exit()
 
 login_adm()
