5adf2044c705f8182110f7a371b342b8870c30d3
Robson Arthur <robsonarthur995@gmail.com>
2026-08-16 00:15:38 -0300

    refactor: organizando a função exibirReservas

--- reserva/exibirReservas.py
+++ reserva/exibirReservas.py
@@ -1,11 +1,16 @@
+import os
+import sys
 
+caminho_absoluto = os.path.abspath(os.curdir)
+sys.path.insert(0, caminho_absoluto)
+import database.reservationDatabase
 
 def exibirReservas():
     print("Exibindo reservas...")
-    reservas = listarReservas()
-    print("="*20)
-    print("NO. CPF        CHEK-IN         CHECK-OUT         VALOR             STATUS")
+    reservas = database.reservationDatabase.listarReservas()
+    print("="*66)
+    print("|NO.  |CPF           |CHEK-IN     |CHECK-OUT   |VALOR |STATUS    |")
     for i in reservas:
-        print(f"{reservas[i]['room_number']:<4} {reservas[i]['guest_cpf']:<13} {reservas[i]['checkin_date']:<11} {reservas[i]['checkout_date']:<11} {reservas[i]['total_value']:<4} {reservas[i]['status']:<10}")
-    print("="*20)
+        print(f"|{i['room_number']:<4} |{i['guest_cpf']:<13} |{i['checkin_date']:<11} |{i['checkout_date']:<11} |{i['total_value']:<4} |{i['status']:<10}|")
+    print("="*66)
 exibirReservas()
