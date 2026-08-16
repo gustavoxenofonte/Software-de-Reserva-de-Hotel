f5a0cb91acd3bcd5a4e9edf59ee04c6fafb19d18
Robson Arthur <robsonarthur995@gmail.com>
2026-08-16 15:51:49 -0300

    feat: adicionado opção para sair e mudança na lógica

--- reserva/cadastroReservas.py
+++ reserva/cadastroReservas.py
@@ -7,31 +7,53 @@ import database.roomsDatabase
 import database.reservationDatabase
 
 def cadastroReserva():
+        print("="*50)
+        print("MENU CADASTRO DE RESERVA")
+        print("Insira 0 a qualquer momento para sair desse menu")
+        print("="*50)
+        print()
 
         cpfHospede = str(input("Insira o CPF do hóspede: ")).strip()
         cpfHospede = "".join(x for x in cpfHospede if x.isnumeric()) ##essa função .join() retira todo caracter que não for númerico da str, como !., e etc.
+        if cpfHospede == '0':
+            return print("Fim do programa")
         while len(cpfHospede) != 11:  ##confere se o cpf é real
             print("CPF inválido")
             cpfHospede = str(input("Insira o CPF do hóspede: ")).strip()
             cpfHospede = "".join(x for x in cpfHospede if x.isnumeric())
+            if cpfHospede == '0':
+                return print("Fim do programa")
+        if database.reservationDatabase.reservaExiste(cpfHospede):
+            print("Erro. CPF já existe no branco de dados de reservas.")
+            return print("Fim do programa")
 
         ## criar condição para testar se o hóspede já existe no banco de dados, se sim, não executar os inputs dos dados abaixo
 
         nomeHospede = str(input("Insira o nome do hóspede que deseja fazer a reserva: ")).strip().title()
+        if nomeHospede == '0':
+            return print("Fim do programa")
 
         telefoneHospede = str(input("Insira o telefone do hóspede: ")).strip()
         telefoneHospede = "".join(x for x in telefoneHospede if x.isnumeric()) ##realiza o mesmo que na variável cpf
+        if telefoneHospede == '0':
+            return print("Fim do programa")
         while len(telefoneHospede) != 11:
             print("Telefone inválido")
             telefoneHospede = str(input("Insira o telefone do hóspede: ")).strip()
             telefoneHospede = "".join(x for x in telefoneHospede if x.isnumeric())
+            if telefoneHospede == '0':
+               return print("Fim do programa")
         
         while True:
             try:
                 idadeHospede = int(input("Insira a idade do hóspede: "))
-                while idadeHospede <= 0:
+                if idadeHospede == 0:
+                    return print("Fim do programa")
+                while idadeHospede < 0:
                     print("Idade inválida")
                     idadeHospede = int(input("Insira a idade do hóspede: "))
+                    if idadeHospede == 0:
+                        return print("Fim do programa")
                 break
             except ValueError:
                 print("Valor inválido")
@@ -41,11 +63,14 @@ def cadastroReserva():
         while True:
             try:         
                 numeroQuarto = int(input("Número do quarto a ser reservado: "))
-                while numeroQuarto <= 0:
+                if numeroQuarto == 0:
+                    return print("Fim do programa")
+                while numeroQuarto < 0:
                     print("Número de quarto inválido")
                     numeroQuarto = int(input("Número do quarto a ser reservado: "))
+                    if numeroQuarto == 0:
+                        return print("Fim do programa")
 
-                existe = False
                 if database.roomsDatabase.quartoExiste(numeroQuarto):           #checa se o quarto existe no banco de dados
                     quartos = database.roomsDatabase.listarQuartos()            #salva uma lista com todos os quartos   
                     for quarto in quartos:
@@ -68,10 +93,14 @@ def cadastroReserva():
         while True:
             try:
                 dataCheckin = str(input("Insira o a data de check-in(DD/MM/YYYY): ")).strip()
+                if dataCheckin == '0':
+                    return print("Fim do programa")
                 dataCheckin = datetime.date.strptime(f"{dataCheckin}", '%d/%m/%Y')    #transforma o str que o usuário digitou em uma variável do tipo date
                 while dataCheckin < datetime.date.today():                             #verifica se a data de check-in é válida(apartir do próximo dia)
                     print("Data de check-in inválida")
                     dataCheckin = str(input("Insira o a data de check-in(DD/MM/YYYY): ")).strip()
+                    if dataCheckin == '0':
+                        return print("Fim do programa")
                     dataCheckin = datetime.date.strptime(f"{dataCheckin}", '%d/%m/%Y')   
                 break 
             except ValueError:
@@ -80,17 +109,21 @@ def cadastroReserva():
         while True:
             try:
                 dataCheckout = str(input("Insira o a data de check-out(DD/MM/YYYY): ")).strip()
+                if dataCheckout == '0':
+                    return print("Fim do programa")
                 dataCheckout = datetime.date.strptime(dataCheckout, "%d/%m/%Y")
                 while dataCheckout < dataCheckin:                      #verifica se a data de check-out é antes da data de check-in, se sim, retorna um erro
                     print("Data de check-out inválida")
                     dataCheckout = str(input("Insira o a data de check-out(DD/MM/YYYY): ")).strip()
+                    if dataCheckout == '0':
+                        return print("Fim do programa")
                     dataCheckout = datetime.date.strptime(dataCheckout, "%d/%m/%Y")
                 break
             except ValueError:
                 print("Valor inválido")
 
         diasHospedados = dataCheckout - dataCheckin
-        valorTotal = diasHospedados.days * valorquartoAtual  ##pega a variável diasHospedados em um número inteiro
+        valorTotal = (1 + diasHospedados.days) * valorquartoAtual  ##pega a variável diasHospedados em um número inteiro
 
         if dataCheckin == datetime.date.today() and quartoAtualReservado == True:        #confere se o quarto atual pode receber o check-in
             database.reservationDatabase.cadastrarReserva(numeroQuarto, cpfHospede, dataCheckin, dataCheckout, valorTotal, 'hospedado')
