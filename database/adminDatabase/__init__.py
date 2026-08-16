82d9969d577853bc06e85aa42249146e055fadf1
guilhermefrmdev <guilhermefrmdev@gmail.com>
2026-08-16 12:02:29 -0300

    feat: commit inicial com criação dos arquivos

---
+++ database/adminDatabase/__init__.py
@@ -0,0 +1,14 @@
+# Verificação se o banco de dados está criado
+try:
+    nome_arquivo = './database/adminDatabase/adminDatabase.csv'
+    arquivo = open(nome_arquivo, 'r+', encoding='utf-8')
+except FileNotFoundError:   # Se não existente, criar
+    arquivo = open(nome_arquivo, 'w+', encoding='utf-8')
+    arquivo.writelines('user_name,password\n')
+arquivo.close()
+
+from .cadastrarLoginAdministrativo import *
+from .alterarSenha import *
+from .alterarUserName import *
+
+__all__ = ['cadastrarLoginAdministrativo', 'alterarSenha', 'alterarUserName']
