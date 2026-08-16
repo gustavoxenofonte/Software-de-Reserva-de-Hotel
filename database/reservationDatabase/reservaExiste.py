ee57e7b50bebecc0dc3640bc954ea0eaf4aa976d
guilhermefrmdev <guilhermefrmdev@gmail.com>
2026-08-15 21:57:08 -0300

    feat: adicionado o módulo reservaExiste

---
+++ database/reservationDatabase/reservaExiste.py
@@ -0,0 +1,23 @@
+from csv import DictReader
+
+class CPFAreNotNumbers(Exception):
+    pass
+
+class CPFAreNotCorrect(Exception):
+    pass
+
+def reservaExiste(guest_cpf: str) -> bool:
+    if not guest_cpf.isdecimal():    # Verifica se o cpf são somente números
+        raise CPFAreNotNumbers("O cpf contém outros caracteres que não são números")
+
+    if len(guest_cpf) != 11:    # Verifica se o cpf tem 11 caracteres
+        raise CPFAreNotCorrect("O cpf não está correto, não tem 11 números")
+
+    with open("./database/reservationDatabase/reservationDatabase.csv", "r", encoding='utf-8') as database:
+            reader = DictReader(database)
+
+            for row in reader:
+                 if row['guest_cpf'] == guest_cpf:
+                      return True                      # Se houver uma reserva no cpf fornecido retorna True
+
+    return False                                       # Senão, retorna False
