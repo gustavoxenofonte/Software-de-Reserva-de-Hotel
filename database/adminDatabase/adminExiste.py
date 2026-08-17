13fd1fa969551acb75aaecfb1b7cdb36e945fcaf
guilhermefrmdev <guilhermefrmdev@gmail.com>
2026-08-17 14:42:33 -0300

    fix: adminExiste agora retorna o login administrativo

--- database/adminDatabase/adminExiste.py
+++ database/adminDatabase/adminExiste.py
@@ -1,10 +1,13 @@
 from csv import reader
 
+class AdminLoginDontExists(Exception):
+    pass
+
 def adminExiste() -> bool:
     try:
         with open("./database/adminDatabase/adminDatabase.csv", "r", encoding='utf-8') as database:
             read = list(reader(database))
             admin = read[1]                # Tenta adcionar o login administrativo a uma variável
-        return True                        # Se conseguir, retorna True
+        return admin                        # Se conseguir, retorna a lista
     except:
-        return False       # Se der erro, retorna false
+        raise AdminLoginDontExists("O login administrativo não existe")      # Se der erro, retorna o erro de login inexistente
