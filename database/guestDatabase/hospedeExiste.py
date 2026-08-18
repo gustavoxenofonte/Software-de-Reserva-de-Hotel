105fc1c57c4f178eb2c22c26e46e22525c9836c9
guilhermefrmdev <guilhermefrmdev@gmail.com>
2026-08-17 22:29:40 -0300

    feat: criado o módulo hospedeExiste

---
+++ database/guestDatabase/hospedeExiste.py
@@ -0,0 +1,25 @@
+from csv import DictReader
+
+class CPFAreNotNumbers(Exception):
+    pass
+
+class CPFAreNotCorrect(Exception):
+    pass
+
+class GuestDontExists(Exception):
+    pass
+
+def reservaExiste(guest_cpf: str) -> bool:
+    if not guest_cpf.isdecimal():    # Verifica se o cpf são somente números
+        raise CPFAreNotNumbers("O cpf contém outros caracteres que não são números")
+
+    if len(guest_cpf) != 11:    # Verifica se o cpf tem 11 caracteres
+        raise CPFAreNotCorrect("O cpf não está correto, não tem 11 números")
+
+    with open("./database/guestDatabase/guestDatabase.csv", "r", encoding='utf-8') as database:
+            reader = DictReader(database)
+
+            for row in reader:
+                if row['cpf'] == guest_cpf:
+                    return row                         # Se houver o cpf no banco de dados retorna o hóspede
+    raise GuestDontExists("O hóspede não existe")      # Senão, dispara um erro
